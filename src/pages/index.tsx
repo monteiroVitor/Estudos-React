a8d492f30a3e90ddd7d7e89729e8ca6b97b36c4f
Vitor Monteiro Mendonça <vitor.mendonca@solutis.com.br>
2021-07-14 10:49:23 -0300

    Feat: Search

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,22 @@
 import Head from "next/head";
+import { useState, FormEvent } from "react";
+import { SearchResults } from "../components/SearchResults";
 
 export default function Home() {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    if (!search.trim()) return;
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`);
+    const data = await response.json();
+
+    setResults(data);
+  }
+
   return (
     <div>
       <Head>
@@ -9,7 +25,20 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1>Somente um teste</h1>
+        <h1>Search</h1>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button type="submit">Buscar</button>
+        </form>
+
+        <div>
+          <SearchResults results={results} />
+        </div>
       </main>
     </div>
   );
