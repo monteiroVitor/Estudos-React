ecb1ea1457bd278e36ec3b987498aedacbe559d5
Vitor Monteiro Mendonça <vitor.mendonca@solutis.com.br>
2021-07-14 15:44:44 -0300

    Feat: memo, useMemo, useCallback

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,15 +1,43 @@
+import { useMemo, useCallback } from "react";
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
   results: Array<{ id: number; price: number; title: string }>;
+  totalPrice: number;
 }
 
-export function SearchResults({ results }: SearchResultsProps) {
+export function SearchResults({ results, totalPrice }: SearchResultsProps) {
+  //* Exemplo useMemo
+  // const totalPrice = useMemo(() => {
+  //   return results.reduce((acc, product) => {
+  //     return acc + product.price;
+  //   }, 0);
+  // }, [results]);
+
+  //* Exemplo useCallback
+  // const handleClick = useCallback(async () => {
+  //   console.log();
+  // }, []);
+
   return (
     <div>
+      <p>{totalPrice}</p>
       {results?.map((product) => (
         <ProductItem key={product.id} product={product} />
       ))}
     </div>
   );
 }
+
+/**
+ ** useMedo, quando usar ?
+ * 1. Cálculos pesados
+ * 2. Igualdade referencial: ao passar uma informação a um componente filho a comparação é rasa, a cada renderização novo espaço de memoria
+ * é alocado para armazenar o dado
+ ** useCallback, quando usar ?
+ * 1. Não gerar re-render desnecessários em componentes que recebem a função como props
+ * Obs: O tamanho/complexidade da função não gera um custo grande no re-render como eu pensava, então não preciso usar useCallback na tentativa
+ * de tornar o render mais rápido.
+ ** Porque removi o userMedo
+ * É melhor formatar e calcular esses após o termino do fetch, pq tenho certeza que ele ocorre uma única vez e não força um re-render.
+ */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,17 @@ import Head from "next/head";
 import { useState, FormEvent } from "react";
 import { SearchResults } from "../components/SearchResults";
 
+type Results = {
+  products: Array<{ id: number; price: number; title: string }>;
+  totalPrice: number;
+};
+
 export default function Home() {
   const [search, setSearch] = useState("");
-  const [results, setResults] = useState([]);
+  const [results, setResults] = useState<Results>({
+    products: [],
+    totalPrice: 0,
+  });
 
   async function handleSubmit(event: FormEvent) {
     event.preventDefault();
@@ -14,7 +22,18 @@ export default function Home() {
     const response = await fetch(`http://localhost:3333/products?q=${search}`);
     const data = await response.json();
 
-    setResults(data);
+    //*Formatando preço
+    // const formatter = new Intl.NumberFormat("pt-BR", {
+    //   style: "currency",
+    //   currency: "pt-BR",
+    // });
+    // formatter.format(product.price)
+
+    const totalPrice = data?.reduce((acc, product) => {
+      return acc + product.price;
+    }, 0);
+
+    setResults({ totalPrice, products: data });
   }
 
   return (
@@ -37,9 +56,27 @@ export default function Home() {
         </form>
 
         <div>
-          <SearchResults results={results} />
+          <SearchResults
+            results={results.products}
+            totalPrice={results.totalPrice}
+          />
         </div>
       </main>
     </div>
   );
 }
+
+/**
+ ** Momentos que uma nova renderização ocorre
+ * 1. Propriedade foi modificada
+ * 2. Componente "Pai" foi modificado
+ * 3. States foram modificados
+ * Obs: "Pai" for modificado ? se sim, todos os filhos serão renderizados novamente)
+ ** Nova Renderização
+ * 1. Cria uma nova versão do componente
+ * 2. Comparar com a versão anterior
+ * 3. Se houverem alterações, vai atualizar o que alterou
+ ** Links interessantes:
+ * 1. https://pt-br.reactjs.org/docs/reconciliation.html
+ * 2. https://pt-br.reactjs.org/docs/rendering-elements.html (sempre bom revisar)
+ */
