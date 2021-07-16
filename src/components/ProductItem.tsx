38eecb0a41098ff9e7437090dbbc1430638aba1e
Vitor Monteiro Mendonça <vitor.mendonca@solutis.com.br>
2021-07-15 22:16:48 -0300

    Feat: Dynamic import

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,4 +1,10 @@
-import { memo } from "react";
+import { memo, useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+import dynamic from "next/dynamic";
+
+const ProductOptions = dynamic(() => {
+  return import("./ProductOptions").then((mod) => mod.ProductOptions);
+});
 
 import styles from "../styles/components/ProductItem.module.scss";
 
@@ -11,6 +17,8 @@ interface ProductItemProps {
 }
 
 function ProductItemComponent({ product }: ProductItemProps) {
+  const [showOptions, setShowOptions] = useState(false);
+
   return (
     <article className={styles.container}>
       <div className={styles.itemContainer}>
@@ -18,8 +26,11 @@ function ProductItemComponent({ product }: ProductItemProps) {
           <strong>{product.title}</strong>
           {product.price}
         </p>
-        <button type="button">opções</button>
+        <button type="button" onClick={() => setShowOptions(!showOptions)}>
+          {showOptions ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
       </div>
+      {showOptions && <ProductOptions />}
     </article>
   );
 }
@@ -37,4 +48,16 @@ export const ProductItem = memo(
  * 2. Renders too often
  * 3. Re-render with same props
  * 4. Medium to big size
+ ** Dynamic import (Code Splitting)
+ * 1. Permite realizar import somente quando necessário, no meu exemplo o import ocorre somente quando o usuário
+ * clica no botão para exibir as opções (linha 29-31).
+ * 2. Ele aceita generics, então caso o meu componente tenha props e tipagem definida posso fazer
+ * dynamic<MinhaTipagem> e sucesso.
+ * 3. Como esse import será feito de forma dinâmica o código será solicitado ao servidor, o que pode demorar um pouco.
+ * Para solucionar esse problema, passo um objeto como atributo "loading" segundo parâmetro da função dynamic.
+ * Ex:
+ *  {
+ *    loading: () =><span>Loading...</span>
+ *  }
+ * Link explicando um pouco melhor (só um pouco): https://pt-br.reactjs.org/docs/code-splitting.html
  */
---
+++ src/components/ProductOptions.tsx
@@ -0,0 +1,12 @@
+import styles from "../styles/components/ProductOptions.module.scss";
+
+export function ProductOptions() {
+  return (
+    <div className={styles.optionsContainer}>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
+        mollitia!
+      </p>
+    </div>
+  );
+}
